d80a4df1271d07d01f5bec712c474519106ad20a
casiacob <casiacob@gmail.com>
2024-01-19 13:59:10 +0200

    load data

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import linear_model_jax
 import matplotlib.pyplot as plt
 from jax import config
-import jax
 
 config.update("jax_enable_x64", True)
 config.update("jax_platform_name", "cpu")
 
 model = linear_model_jax.LinearModel()
 xy = model.genData(50)
+# xy = numpy.load('data.npy')
+# xy = jnp.array(xy)
+# import jax.numpy as jnp
+# import numpy
 fig, ax = plt.subplots(figsize=(7, 7))
 ax.plot(xy[0, :], xy[1, :], label="Connecting lines", color="b", linestyle="--")
 ax.scatter(xy[0, :], xy[1, :], label="Tracked points", color="r")
